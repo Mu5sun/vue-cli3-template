de85bc0a250b13b53f210af0267a095fbfcb66f3
Mu5sun <musun.lin@aliyun.com>
2019-04-17 19:09:49 +0800

    vue.config.js 调整

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,45 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    'postcss-sprites': { // 可以生成雪碧图，使用的时候直接引入小图片，该配置会自动把小图片合成
+      basePath: './dist',
+      spritePath: './dist/images/sprites',
+      retina: true,
+      filterBy: function (image) {
+        // 只合成 sprites 目录下的图片，并且是 .png 为后缀
+        if (image.url.includes('sprites')) {
+          if (!/\.png$/.test(image.url)) {
+            return Promise.reject(new Error('Allow only png files'))
+          }
+          return Promise.resolve()
+        }
+        return Promise.reject(new Error('Allow only png files'))
+      }/* ,
+       该配置是在小图片很多的情况下，分模块打包
+      groupBy: function (image) {
+        const spritesPaths = image.url.split('sprites')
+        if (spritesPaths.length > 1) {
+          const spriteImagePaths = spritesPaths[1].split('/')
+          if (spriteImagePaths.length > 2) {
+            const groupName = spriteImagePaths[1]
+            return Promise.resolve(groupName)
+          } else {
+            return Promise.reject(new Error('Not a group name'))
+          }
+        } else {
+          return Promise.reject(new Error('Not a group name'))
+        }
+      } */
+    },
+    'postcss-px-to-viewport': { // 该插件可以将px转换成vw，注意兼容
+      viewportWidth: 750, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750 （如果我们设置的宽度是300px，那么编译之后的宽度为(300/750*100)=40vw,如果频宽实际为375px，那么该元素的宽度为（375*0.4）= 150px）
+      viewportHeight: 1334, // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
+      unitPrecision: 5, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除)
+      viewportunit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
+      selectorBlackList: [], // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
+      minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
+      mediaQuery: false, // 允许在媒体查询中转换`px`
+      propList: ['*'] // 可以指定不转换的属性，*代表全部转换
+    }
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -2,22 +2,54 @@
 const merge = require('webpack-merge')
 const path = require('path')
 const CompressionPlugin = require('compression-webpack-plugin')
+const IS_PROD = process.env.NODE_ENV === 'production'
 
 const resolve = dir => {
   return path.join(__dirname, dir)
 }
 
 module.exports = {
-  configureWebpack: () => ({
-    devtool: 'source-map'
-  }),
+  publicPath: IS_PROD ? '' : '', // 配置打包后的部署路径
+  outputDir: 'dist', // 配置打包后的文件夹名称，默认就是dist
+  assetsDir: 'assets', // 配置打包后放置静态资源的文件夹
+  indexPath: 'index.html', // 配置生成的index.html模版的输出路径，默认是index.html,相对于打包后的主文件夹
+  filenameHashing: true, // 配置打包后的文件名进行hash，要求index的html不是cli生成的。如果不是，请改为false
+  pages: undefined, // 配置多页应用的入口和出口，单页应用不用配置
+  lintOnSave: !IS_PROD, // 配置在开发环境时，进行代码检测
+  runtimeCompiler: false, // 配置是否能在vue组件中使用template选项，如果是true，可以使用，但会增加文件大小，不建议
+  transpileDependencies: [], // 配置babel-loader忽略node_modules里的所有文件，如果需要转移其中的依赖，在数组中列出
+  productionSourceMap: true, // 配置文件打包时需不需要生成map文件，供线上环境的代码错误定位
+  crossorigin: undefined, // 配置打包后的html文件中的link和script标签上是否添加crossorigin属性，仅影响构建时注入的文件，不影响模版中的文件
+  integrity: false, // 配置打包后的html文件中的link和script标签上是否添加Subresource Integrity属性，仅影响构建时注入的文件，不影响模版中的文件。开启该属性可以提供在cdn上的文件额外的安全，开启时，preload resource hints 会被禁用，因为Chrome的bug
+  configureWebpack: config => {
+    if (IS_PROD) {
+      // 为生产环境修改配置
+    } else {
+      // 为开发环境修改配置
+    }
+  },
   chainWebpack: config => {
     // 这里是对环境的配置，不同环境对应不用的BASE_URL，以便axios的请求地址不同
     config.plugin('define').tap(args => {
       args[0]['process.env'].BASE_URL = JSON.stringify(process.env.BASE_URL)
       return args
     })
-    if (process.env.NODE_ENV === 'production') {
+    if (IS_PROD) {
+      // #region 图片压缩
+      config.module
+        .rule('images')
+        .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+        .use('img-loader')
+        .loader('img-loader').options({
+          plugins: [
+            require('imagemin-jpegtran')(),
+            require('imagemin-pngquant')({
+              quality: [0.75, 0.85]
+            })
+          ]
+        })
+      // #endregion
+
       // #region 启用GZip压缩，前端使用了GZip压缩，后端也要配合开启GZip
       config.plugin('compression').use(CompressionPlugin, {
         // 目标文件名称。[path] 被替换为原始文件的路径和 [query]查询
@@ -32,6 +64,7 @@ module.exports = {
         minRatio: 0.8,
         cache: true
       })
+      // #endregion
     }
     // #region
     // 将这些指定的文件不进行打包
@@ -73,5 +106,19 @@ module.exports = {
           limit: 5120 // 当图片没有超过指定的大小时，转为base64的文件，保存在静态资源中
         })
       })
+  },
+  devServer: {
+    overlay: { // 可以在控制台同时输出警告和错误
+      warnings: true,
+      errors: true
+    },
+    proxy: {
+      '/api': { // 配置服务器代理
+        target: '',
+        // 如果要代理websocket
+        ws: true,
+        changeOrigin: true
+      }
+    }
   }
 }
